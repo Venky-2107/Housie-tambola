2fefdfc1a46b436c0124ce1072d954436d63c2cf
Venky-2107 <chvenkatpatiraju@gmail.com>
2025-12-07 13:53:40 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Housie-tambola/'   // <-- ADD THIS
+  base: '/housie-tambola/'   // <-- ADD THIS
 })
